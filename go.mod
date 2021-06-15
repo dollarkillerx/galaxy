4816f1d7c302d1edb3a66cdc5591cdaf835151af
dollarkillerx <adapawang@gmail.com>
2021-06-15 18:56:49 +0800

    :construction: improve mq manager

--- go.mod
+++ go.mod
@@ -7,5 +7,5 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/pingcap/errors v0.11.5-0.20201126102027-b0a155152ca3
-	github.com/satori/go.uuid v1.2.0
+	github.com/pkg/errors v0.9.1
 )
--- internal/mq_manager/manager.go
+++ internal/mq_manager/manager.go
@@ -1,1 +1,69 @@
 package mq_manager
+
+import (
+	"fmt"
+
+	"github.com/dollarkillerx/galaxy/internal/mq_manager/es"
+	"github.com/dollarkillerx/galaxy/internal/mq_manager/kafka"
+	"github.com/dollarkillerx/galaxy/internal/mq_manager/mongodb"
+	"github.com/dollarkillerx/galaxy/internal/mq_manager/nsq"
+	"github.com/dollarkillerx/galaxy/pkg"
+	"github.com/pkg/errors"
+)
+
+type manager struct {
+	mq map[string]MQ
+}
+
+var Manager = manager{
+	mq: map[string]MQ{},
+}
+
+// TODO: 处理重复问题
+func (m *manager) Register(task pkg.Task) error {
+	switch {
+	case task.KafkaConf != nil:
+		kfk := kafka.Kafka{}
+		err := kfk.InitMQ(task)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		m.mq[task.TaskBaseData.TaskID] = &kfk
+	case task.NsqConf != nil:
+		nq := nsq.NSQ{}
+		err := nq.InitMQ(task)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		m.mq[task.TaskBaseData.TaskID] = &nq
+	case task.MongoDBConf != nil:
+		mongo := mongodb.MongoDB{}
+		err := mongo.InitMQ(task)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		m.mq[task.TaskBaseData.TaskID] = &mongo
+	case task.ESConf != nil:
+		e := es.ES{}
+		err := e.InitMQ(task)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		m.mq[task.TaskBaseData.TaskID] = &e
+	}
+
+	return nil
+}
+
+func (m *manager) Get(taskID string) (MQ, error) {
+	mq, ex := m.mq[taskID]
+	if !ex {
+		return nil, errors.New(fmt.Sprintf("not exits: %s", taskID))
+	}
+
+	return mq, nil
+}
